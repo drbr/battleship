f9136fb18e1afa2416852e8a045aae92d4601526
Andrew Brandon <drbr@users.noreply.github.com>
2017-02-19 23:17:49 -0800

    Added a mouse hover effect on the cells

--- src/boardComponents/Cell.js
+++ src/boardComponents/Cell.js
@@ -3,20 +3,31 @@ import React, { Component } from 'react';
 const CELL_SIZE = 30;
 
 const DEFAULT_STYLE = {
-  border: '2px solid black',
   height: CELL_SIZE,
   width: CELL_SIZE
 };
 
 export class Cell extends Component {
+  state = { hovered: false };
+
+  boundOnMouseEnter = () => this.setState({ hovered: true });
+  boundOnMouseLeave = () => this.setState({ hovered: false });
+
   render() {
     let style = {
-      backgroundColor: this.props.hit ? 'red': null,
+      backgroundColor: (this.props.hit || this.state.clicked) ? 'rgb(255, 0, 0)' : null,
+      border: this.state.hovered ? '2px solid black' : '2px solid transparent',
       ...DEFAULT_STYLE
     };
-    return <div style={style} onClick={this.props.onClick}>
-      { this.props.shipId }
-    </div>;
+
+    return (
+      <div style={style}
+           onClick={this.props.onClick}
+           onMouseEnter={this.boundOnMouseEnter}
+           onMouseLeave={this.boundOnMouseLeave} >
+        { this.props.shipId }
+      </div>
+    );
   }
 }
 
